7be688b6aa301ee017e54e336a99c16798020408
Cooper Knaak <cooperknaak@gmail.com>
2019-01-11 13:36:48 -0600

    Updated version of CoronaErrors.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/auth-provider.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/CooperCorona/CoronaErrors.git", from: "1.0.0")
+        .package(url: "https://github.com/CooperCorona/CoronaErrors.git", from: "2.0.0")
     ],
     targets: [
         .target(
