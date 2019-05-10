6a12c2a0d7f7785a4336fadc9cd1f0c51808a9c5
Cooper Knaak <cooperknaak@gmail.com>
2019-05-10 15:01:11 -0500

    Change DatabaseConnectable to DatabaseConnectionPool.

--- Sources/Roles/RolesAccessor.swift
+++ Sources/Roles/RolesAccessor.swift
@@ -11,37 +11,47 @@ import Authentication
 
 ///Accesses role and manages roles info in the data store
 ///for a given authenticatable and role entity.
-open class RolesAccessor<TRole, TAuth>: RolesAccessorProtocol where TRole: RoleIdentifier, TAuth: Model, TAuth: Authenticatable {
+open class RolesAccessor<TRole, TAuth>: RolesAccessorProtocol where
+    TRole: RoleIdentifier,
+    TAuth: Model & Authenticatable {
 
-    private let connection:DatabaseConnectable
+    private let connectionPool:DatabaseConnectionPool<TAuth.Database>
 
-    public init(connection:DatabaseConnectable) {
-        self.connection = connection
+    public init(connection:DatabaseConnectionPool<TAuth.Database>) {
+        self.connectionPool = connection
     }
     
     ///Returns true if the given entity has the given role.
     ///Throws an exception on Fluent errors.
     open func entity(entity:TAuth, has role:TRole) throws -> EventLoopFuture<Bool> {
-        return try entity.has(role: role, on: connection)
+        return self.connectionPool.withConnection() { connection in
+            return try entity.has(role: role, on: connection)
+        }
     }
     
     ///Returns true if the entity is associated with *at least 1* role in
     ///roles.includedRoles and is **not** associated with *any* role in
     ///roles.excludedRoles. Throws an exception on Fluent errors.
     public func entity(entity: TAuth, has roles: RolesGroup<TRole>) throws -> EventLoopFuture<Bool> {
-        return try entity.has(roles: roles, on: connection)
+        return self.connectionPool.withConnection() { connection in
+            return try entity.has(roles: roles, on: connection)
+        }
     }
     
     ///Associates a given role with an entity. Throws an exception
     ///if the role is already associated with the entity or on Fluent errors.
     public func add(role: TRole, to entity: TAuth) throws -> EventLoopFuture<Role<TRole, TAuth>> {
-        return try entity.add(role: role, on: connection)
+        return self.connectionPool.withConnection() { connection in
+            return try entity.add(role: role, on: connection)
+        }
     }
     
     ///Disassociates a given role from an entity. Throws an exception
     ///if the role is not already associated with the entity or on Fluent errors.
     public func remove(role: TRole, from entity: TAuth) throws -> EventLoopFuture<Void> {
-        return try entity.remove(role: role, on: connection)
+        return self.connectionPool.withConnection() { connection in
+            return try entity.remove(role: role, on: connection)
+        }
     }
     
 }
