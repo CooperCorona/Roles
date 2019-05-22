00c289598513344603e631fcc87dc69f87c33453
Cooper Knaak <cooperknaak@gmail.com>
2019-05-21 19:04:14 -0500

    Updated RolesAuthenticationMiddleware due to restrictions on using Service and RolesManagerProtocol (which contains associated types).

--- Sources/Roles/RolesAuthenticationMiddleware.swift
+++ Sources/Roles/RolesAuthenticationMiddleware.swift
@@ -13,40 +13,34 @@ import HTTP
 ///Middleware that authorizes authenticatable entities of a certain role(s).
 ///If the entity's roles do not match the given roles group, then a 404 error
 ///is thrown.
-open class RolesAuthenticationMiddleware<TRolesManager>: Middleware where TRolesManager: RolesManagerProtocol {
-    
-    public typealias TRole = TRolesManager.TRole
-    public typealias TAuth = TRolesManager.TAuth
-    
-    ///An error thrown when an entity is authenticated, but does not
-    ///have the proper roles to access a certain resource.
-    public struct RolesError: AbortError {
-        public var identifier: String { return "RolesError" }
-        public var reason: String { return "Unauthorized" }
-        public var status: HTTPResponseStatus { return .unauthorized }
-        public var headers: HTTPHeaders { return HTTPHeaders() }
-        
-    }
-    
-    ///Accesses roles information for authenticatable entities.
-    public let rolesManager:TRolesManager
+///
+///NOTE: Until Vapor supports Swift 5.1 with opaque types, there's no way to use Services to create
+///a RolesManagerProtocol instance because it has associated types. Clients are required to subclass
+///RolesAuthenticationMiddleware and implement the `entity(entity:has:)` method, create a concrete
+///instance of RolesManagerProtocol, and calling `entity(entity:has:)` on it. This restriction will
+///be lifted once opaque types are supported.
+open class RolesAuthenticationMiddleware<TRole, TAuth>: Middleware where TRole: RoleIdentifier, TAuth: Authenticatable & Model {
+
     ///The roles that should and should not be allowed to access this resource.
     public let roles:RolesGroup<TRole>
     
     ///Initializes a RolesAuthenticationMiddleware object with a given roles manager and roles group.
-    public init(rolesManager:TRolesManager, roles:RolesGroup<TRole>) {
-        self.rolesManager = rolesManager
+    public init(roles:RolesGroup<TRole>) {
         self.roles = roles
     }
 
-    open func respond(to request: Request, chainingTo next: Responder) throws -> EventLoopFuture<Response> {
-        let auth = try request.requireAuthenticated(TAuth.self)
-        return try self.rolesManager.entity(entity: auth, has: self.roles).flatMap() {
+    public func respond(to request: Request, chainingTo next: Responder) throws -> EventLoopFuture<Response> {
+        let auth:TAuth = try request.requireAuthenticated(TAuth.self)
+        return try self.entity(entity: auth, has: self.roles).flatMap() {
             guard $0 else {
-                throw RolesError()
+                throw Abort(.unauthorized)
             }
             return try next.respond(to: request)
         }
+    }
+
+    open func entity(entity:TAuth, has:RolesGroup<TRole>) throws -> EventLoopFuture<Bool> {
+        fatalError("Not implemented. Subclasses of RolesAuthenticationMiddleware must implement entity(entity:has:).")
     }
     
 }
