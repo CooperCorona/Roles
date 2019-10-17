896375f91a60315f33340246bcdda0b568395b41
Cooper Knaak <cooperknaak@gmail.com>
2019-10-16 21:16:44 -0700

    Updated to Swift 5.

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
       .package(url: "https://github.com/vapor/auth.git", from: "2.0.0"),
       .package(url: "https://github.com/vapor/fluent.git", .upToNextMajor(from: "3.0.0")),
       .package(url: "https://github.com/vapor/fluent-sqlite.git", .upToNextMajor(from: "3.0.0")),
-      .package(url: "https://github.com/CooperCorona/CoronaErrors.git", .branch("Swift42"))
+      .package(url: "https://github.com/CooperCorona/CoronaErrors.git", .branch("Swift5"))
     ],
     targets: [
         .target(
